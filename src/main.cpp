ca038c55bd4958c9ca9c5c1f9a03e292b247cbed
dmlr1996 <getthopapst@hotmail.de>
2021-10-22 21:24:10 +0200

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /* ------------------------------------------
-   This is demo code for the Blink-Funk-Dings
-   Version 0.1, 17.10.2021
-   Author: Tobias Heer
+   Blink-Funk-Dings
+   Version 1.0, 21.10.2021
+   Author: Dominik Magerle
    License: CC BY 4.0 https://creativecommons.org/licenses/by/4.0/
    -------------------------------------------
  */
@@ -9,6 +9,12 @@
 
 #include <ESP8266WiFi.h> // Wifi Library
 #include "FastLED.h"     // LED Library
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+#define MSG_BUFFER_SIZE 50
 
 
 #define BUTTON_PIN 0 
@@ -20,8 +26,24 @@
 CRGB ledstrip[NUM_LEDS]={{0}};
 
 // Network SSID
-const char* ssid     = "BFD";
-const char* password = "dingsdings";
+//const char* ssid     = "BFD";
+//const char* password = "dingsdings";
+
+const char* ssid     = "DOMINIK";
+const char* password = "Dominik1996";
+
+const char* mqtt_server = "broker.mqttdashboard.com";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+unsigned long lastMsg = 0;
+
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+
+
 
 // Button state for toggle button
 unsigned int button_state = LOW;
@@ -31,7 +53,7 @@ void toggle_state(){
   button_state = !button_state;
 }
 
-//Check fpr button presses. This was implemented a bit lazily.
+//Check for button presses. This was implemented a bit lazily.
 void handle_button(unsigned int button_pin)
 {
   static double last_push = 0;
@@ -54,7 +76,7 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT);
 
   FastLED.addLeds<NEOPIXEL, LED_PIN>(ledstrip, NUM_LEDS);
-  FastLED.setBrightness(  BRIGHTNESS );
+  FastLED.setBrightness(BRIGHTNESS);
   
   // Connect WiFi
   Serial.println(WiFi.macAddress());
@@ -72,6 +94,7 @@ void setup() {
   // Print the IP address when conneted
   Serial.print("IP address: ");
   Serial.print(WiFi.localIP());
+  Serial.print("\n");
   ledstrip[2] = CRGB::Green;
   FastLED.show();
 }
@@ -80,24 +103,22 @@ void loop() {
   static int counter  = 0;
   
   handle_button(0);
-  ledstrip[0] = CRGB::Blue;
+  //ledstrip[0] = 255;
   ledstrip[1] = CHSV(counter % 255,255,255);
   ledstrip[3] = (button_state == LOW?CRGB::Black: CRGB(255, 128, 0));
 
 
   // Check WLAN connection (no need to do this as often as we do here! This is just a demo
 
-  if (WiFi.status() != WL_CONNECTED) {
+  if ((WiFi.status() != WL_CONNECTED) && (counter % 2 == 1)) {
     ledstrip[2] = CRGB::Red;
   } else {
     ledstrip[2] = CRGB::Green;
   }
   
-
-  
   FastLED.show();
   counter = counter +1  % 1024;
-  delay(50);
+  delay(100);
     
 }
 
